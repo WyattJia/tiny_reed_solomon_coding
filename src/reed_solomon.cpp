aadee2d3ebe49cd5199acb4b2362cf33cebb4df5
Wyatt Jia <i@eventloop.live>
2022-10-13 00:15:05 +0800

    Create and init src folder.

---
+++ src/reed_solomon.cpp
@@ -0,0 +1,11 @@
+class ReedSolomon {
+
+    int dataShardCount;
+    int parityShardCount;
+    int totalShardCount;
+
+    ReedSolomon();
+
+    void create(int dataShardCount, int parityShardCount);
+
+}; 
